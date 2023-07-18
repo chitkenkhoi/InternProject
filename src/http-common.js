91f47640747cf3536f08037206b7e12e42039d82
chitkenkhoi <117997639+chitkenkhoi@users.noreply.github.com>
2023-07-18 09:01:55 +0700

    Delete src directory

--- src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const HTTP = axios.create({
-    baseURL: `http://localhost:8080/api/`,
-    headers: {
-        'Content-Type': 'application/json;charset=UTF-8'
-    }
-})
-export default HTTP
